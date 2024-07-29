16d902c9460606b2c04c08218c572e50ce3aeede
Gopal Kumar <99078722+Gopal-13@users.noreply.github.com>
2024-07-30 00:58:34 +0530

    Add files via upload

---
+++ Atminterface.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class Atminterface {
+    static void checkBalance(int x) {
+        System.out.println("Your current balance is: " + x);
+    }
+
+    static int deposit(int x, int y) {
+        System.out.print("How much would you like to deposit?");
+
+        int balance = x + y;
+        System.out.println("Your new balance is: " + balance);
+        return balance;
+    }
+
+    static int withdraw(int x, int y) {
+        int balance = x - y;
+        System.out.println(y + " withdrawn from your account. Your balance amount is: " + balance);
+        return balance;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your initial amount:");
+        int amount = sc.nextInt();
+        while (true) {
+            System.out.println("1. Check Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            int choice = sc.nextInt();
+            if (choice == 1) {
+                checkBalance(amount);
+            } else if (choice == 2) {
+                System.out.println("Enter amount to deposite:");
+                int deposit = sc.nextInt();
+                amount = deposit(amount, deposit);
+
+            } else if (choice == 3) {
+                System.out.println("Enter amount to withdraw:");
+                int withdraw = sc.nextInt();
+                amount = withdraw(amount, withdraw);
+            } else if (choice == 4) {
+                break;
+            } else {
+                System.out.println("Invalid choice");
+            }
+
+        }
+
+    }
+}
